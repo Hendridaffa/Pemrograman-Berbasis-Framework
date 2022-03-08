c7c627f9fefa1d61bd9f23e0b35c694df62d2457
Hendridaffa <hendridaffa23@gmail.com>
2022-03-08 16:41:50 +0700

    Update

--- Pertemuan3-React Component/web-reactku/src/index.js
+++ Pertemuan3-React Component/web-reactku/src/index.js
@@ -4,7 +4,7 @@ import Test from './Test';
 import List from './List';
 import HelloComponent from './HelloComponent/HelloComponent';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 class StateFullComponent extends React.Component{
@@ -12,7 +12,7 @@ class StateFullComponent extends React.Component{
     return<p>StateFullComponent</p>
   }
 }
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<Test />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
