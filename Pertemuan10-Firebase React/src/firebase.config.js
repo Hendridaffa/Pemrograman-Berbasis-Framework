6ef488fc484b49ad9df9f2128e9d8749e3c7decc
Hendridaffa <hendridaffa23@gmail.com>
2022-06-02 00:56:25 +0700

    Firebase React

---
+++ Pertemuan10-Firebase React/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+        apiKey: "AIzaSyAw6datezSE6NI7RdmVVa5WsuFrU4pXP8o",
+        authDomain: "hendri-web-291104.firebaseapp.com",
+        projectId: "hendri-web-291104",
+        storageBucket: "hendri-web-291104.appspot.com",
+        messagingSenderId: "496952274483",
+        appId: "1:496952274483:web:f1ce51abac7ea3dec6bd46",
+        measurementId: "G-FP0JC7YFPJ"
+};
