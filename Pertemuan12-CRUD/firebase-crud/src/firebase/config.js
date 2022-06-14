fbcea5e4b56456b55c5391ef56f80421fb0ed4ff
Hendri Daffa <60720676+Hendridaffa@users.noreply.github.com>
2022-06-14 19:45:51 +0700

    CRUD

---
+++ Pertemuan12-CRUD/firebase-crud/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBW__NPkVHpJgO-otqztlkgiIwLPJHaamU",
+    authDomain: "hendri-pbf.firebaseapp.com",
+    databaseURL: "https://hendri-pbf-default-rtdb.firebaseio.com",
+    projectId: "hendri-pbf",
+    storageBucket: "hendri-pbf.appspot.com",
+    messagingSenderId: "837803614434",
+    appId: "1:837803614434:web:2461249c1400cbe3f3bebb",
+    measurementId: "G-Y4ZYVBRJ5C"
+};
+export default firebaseConfig;
